f73796c55b9d6afa4456746bd27bf71d38debe74
bigname <tuanhiep.tran88@gmail.com>
2021-01-10 16:43:25 -0700

    run MHTL for cryotojacking_attack dataset

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # @Xian Teng
 # @2017
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
 
 import SVDD
 from MTHL import MTHL
@@ -23,7 +25,8 @@ def normalize(train_data, test_data):
 
 class Config(object):
     def __init__(self):
-        self.p = 32
+        self.p = 5
+        # self.p = 32
         self.q = 5
         self.lambda1 = 0.12
         self.lambda2 = 1.0
@@ -31,6 +34,15 @@ class Config(object):
         self.s = 2
         self.convergence = 1e-8
         self.tolerance = 1e-1
+
+
+def create_train_data(df_view, nb_time_steps):
+    my_list = []
+    nb_rows = df_view.shape[0]
+    for index in range(0, nb_rows, nb_time_steps):
+        my_list.append(df_view[index:index + nb_time_steps].transpose().to_numpy())
+    mat = np.array(my_list)
+    return mat
 
 
 if __name__ == "__main__":
@@ -64,6 +76,8 @@ if __name__ == "__main__":
     print "test acc = ", test_acc
 
     # test with cryptojacking_attack data
+    nb_instances = 1000
+    nb_time_steps = 10
     nb_views = 3
     dataset_name = "cryptojacking_attack"
     anomaly_rate = 5
@@ -76,12 +90,40 @@ if __name__ == "__main__":
     dataset_path = "{}/{}/anomaly_rate_{}_views_{}/{}".format(ready_data_directory, dataset_name, anomaly_rate,
                                                               nb_views,
                                                               method)
+    # normalize dataset
+    scaler = StandardScaler()
     for i in range(nb_views):
         df_view = pd.read_csv(dataset_path + "/view_%d.csv" % (i + 1),
                               # index_col="Unnamed: 0",
                               # index_col="timestamp",
                               header="infer")
         dim_views[i] = len(df_view.columns)
-        df_views.append(df_view)
-    # pre-process dataset
-    dataset = pd.concat(df_views, axis=1)
+        normalized_df_view_numpy = scaler.fit_transform(df_view.head(nb_time_steps * nb_instances))
+        normalized_df_view = pd.DataFrame(normalized_df_view_numpy)
+        train_data_view_i = create_train_data(normalized_df_view, nb_time_steps)
+        df_views.append(train_data_view_i)
+    # ground truth
+    ground_truth = pd.read_csv(dataset_path + "/ground_truth.csv", header="infer")
+
+    # train_data is of dimension: nb_views x nb_instances x nb_features x nb_time_step
+    train_data = df_views
+    train_label = ground_truth["is_anomaly"].head(nb_instances)
+
+    # .....run TSHL ...... #
+    start_time = time.time()
+    tshl = MTHL(train_data, config)
+    tshl.Optimization(1e7)
+    end_time = time.time()
+    print "it costs", float(end_time - start_time), "seconds."
+
+    # # predict test data
+    # train_score = tshl.predict(train_data, fg="score")
+    # auc = metrics.roc_auc_score(train_label, train_score)
+    # print "auc = ", auc
+
+    # predict test data
+    train_pred = tshl.predict(train_data, fg="label")
+
+    train_acc = accuracy_score(train_label, train_pred, normalize=True)
+    print "train acc = ", train_acc
+
